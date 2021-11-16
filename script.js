c21ea6baf2479dcdf20b979697dd08a633bf438d
Arjun kumar <93057535+arjun-kumar-nit@users.noreply.github.com>
2021-11-16 18:05:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a = prompt("Enter UserName");
 var b = prompt("Enter password");
-if (a == "jayesh" && b == "poonam") {
+if (a == "jayesh" && b == "123") {
     window.location.href = "start.html";
 } else {
     alert("invalid details");
